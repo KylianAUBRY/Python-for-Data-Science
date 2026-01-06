305b66821ae6afc97b249ea4f4b1f2c4abe05141
kyaubry <aubry.kylian973@gmail.com>
2026-01-06 18:05:25 +0100

    finish

--- python0/ex09/setup.py
+++ python0/ex09/setup.py
@@ -4,8 +4,10 @@ setup(
     name="ft_package",
     version="0.0.1",
     description="My first python package from 42 project",
-    url="https://github.com/KylianAUBRY/Python-for-Data-Science",
+    url="https://github.com/KylianAUBRY/Python-for-Data-Science/\
+        tree/main/python0/ex09/ft_package",
     author="kyaubry",
     author_email="kyaubry@student.42lehavre.fr",
+    license="MIT",
     packages=find_packages(),
 )
